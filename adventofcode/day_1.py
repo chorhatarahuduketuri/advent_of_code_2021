0992322d688798ea59a0a09d867c6c146ccf6438
chhk <chorhatarahuduketuri@gmail.com>
2021-12-01 22:55:52 +0000

    Different formatting - probably worse

--- adventofcode/day_1.py
+++ adventofcode/day_1.py
@@ -4,12 +4,17 @@ def read_input():
 
 
 def first_star(sonar_readings):
-    print(sum([1 if sonar_reading > sonar_readings[i - 1] else 0 for i, sonar_reading in enumerate(sonar_readings)]))
+    print(sum([1 if sonar_reading > sonar_readings[i - 1]
+               else 0
+               for i, sonar_reading
+               in enumerate(sonar_readings)]))
 
 
 def second_star(sonar_readings):
-    print(sum([1 if (i > 2) and (sum(sonar_readings[i - 3:i]) > sum(sonar_readings[i - 4:i - 1])) else 0 for
-               i, sonar_reading in enumerate(sonar_readings)]))
+    print(sum([1 if (i > 2) and (sum(sonar_readings[i - 3:i]) > sum(sonar_readings[i - 4:i - 1]))
+               else 0
+               for i, sonar_reading
+               in enumerate(sonar_readings)]))
 
 
 def main():
