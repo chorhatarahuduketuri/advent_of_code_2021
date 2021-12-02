f00b7f031a9a9ad8f439444706855215f77b431d
chhk <chorhatarahuduketuri@gmail.com>
2021-12-02 07:45:04 +0000

    Test for day 2 star 1 with layout copied from day 1 tests

--- tests/test_day_2.py
+++ tests/test_day_2.py
@@ -1,2 +1,10 @@
-day_2_test_input = []
+import pytest
 
+day_2_star_1_test_input = [([('forward', 5), ('down', 5), ('forward', 8), ('up', 3), ('down', 8), ('forward', 2)], 150)]
+
+
+@pytest.mark.parametrize(["input", "output"], day_2_star_1_test_input)
+def test_day_2_star_1(capsys, input, output):
+    first_star(input)
+    captured = capsys.readouterr()
+    assert int(captured.out) == 150
