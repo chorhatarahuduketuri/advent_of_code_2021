e55fece6a86b1321843646664513251df1c707bb
chhk <chorhatarahuduketuri@gmail.com>
2021-12-02 07:14:52 +0000

    Correcting variable name

--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -2,17 +2,17 @@ import pytest
 
 from day_1 import first_star, second_star
 
-day_1_star_1 = [([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7)]
+day_1_test_input = [([199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 7)]
 
 
-@pytest.mark.parametrize(["input", "output"], day_1_star_1)
+@pytest.mark.parametrize(["input", "output"], day_1_test_input)
 def test_day_1_star_1(capsys, input, output):
     first_star(input)
     captured = capsys.readouterr()
     assert int(captured.out) == 7
 
 
-@pytest.mark.parametrize(["input", "output"], day_1_star_1)
+@pytest.mark.parametrize(["input", "output"], day_1_test_input)
 def test_day_1_star_2(capsys, input, output):
     second_star(input)
     captured = capsys.readouterr()
