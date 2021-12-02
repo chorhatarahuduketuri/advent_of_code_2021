7208a8991f022d02b3436144658b3f9d6c3fa8b7
chhk <chorhatarahuduketuri@gmail.com>
2021-12-02 08:06:26 +0000

    Adding day 2 star 2 test

--- tests/test_day_2.py
+++ tests/test_day_2.py
@@ -10,3 +10,13 @@ def test_day_2_star_1(capsys, input, output):
     first_star(input)
     captured = capsys.readouterr()
     assert int(captured.out) == output
+
+
+day_2_star_2_test_input = [([('forward', 5), ('down', 5), ('forward', 8), ('up', 3), ('down', 8), ('forward', 2)], 900)]
+
+
+@pytest.mark.parametrize(["input", "output"], day_2_star_2_test_input)
+def test_day_2_star_2(capsys, input, output):
+    second_star(input)
+    captured = capsys.readouterr()
+    assert int(captured.out) == output
