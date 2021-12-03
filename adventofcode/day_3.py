7de8c7231569d5baf95b17d36fe707b2d1d69781
chhk <chorhatarahuduketuri@gmail.com>
2021-12-03 08:01:54 +0000

    Day 3 star 1

--- adventofcode/day_3.py
+++ adventofcode/day_3.py
@@ -4,7 +4,21 @@ def get_readings():
 
 
 def first_star(readings):
-    pass
+    counters = dict()
+    for i in range(len(readings[0])):
+        counters[i] = [0, 0]
+    for reading in readings:
+        for i, bit in enumerate(reading):
+            if bit == '0':
+                counters[i][0] += 0
+                counters[i][1] += 1
+            else:
+                counters[i][0] += 1
+                counters[i][1] += 0
+    counted = [[counter.index(max(counter)), counter.index(min(counter))] for k, counter in counters.items()]
+    gamma = int(''.join([str(count[0]) for count in counted]), 2)
+    epsilon = int(''.join([str(count[1]) for count in counted]), 2)
+    print(gamma * epsilon)
 
 
 def second_star(readings):
