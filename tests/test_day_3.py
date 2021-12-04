970c37dd9721e30e1abcf425c8f52c4b081c0ff2
chhk <chorhatarahuduketuri@gmail.com>
2021-12-04 11:45:58 +0000

    Adding day 3 star 1 testing input

--- tests/test_day_3.py
+++ tests/test_day_3.py
@@ -2,7 +2,21 @@ import pytest
 
 from day_3 import first_star, second_star
 
-day_3_star_1_test_input = [(['something'], 198)]
+day_3_star_1_test_input = [
+    (['00100',
+      '11110',
+      '10110',
+      '10111',
+      '10101',
+      '01111',
+      '00111',
+      '11100',
+      '10000',
+      '11001',
+      '00010',
+      '01010'],
+     198)
+]
 
 
 @pytest.mark.parametrize(["input", "output"], day_3_star_1_test_input)
